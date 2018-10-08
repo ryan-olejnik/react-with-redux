477029b6509fca2ffabd11f2767c7617a21bd83e
Ryan Olejnik <ryan@paddlehr.com>
2018-10-07 21:50:27 -0400

    Add redux-saga middleware to createStore()

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import App from './App';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+
+// sagaMiddleware.run()
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
